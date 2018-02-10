52aedffe49d558127900b06d066117c3e416fada
kei-p <kei-p@sonicgarden.jp>
2018-02-10 13:01:15 +0900

    Add Asynchronously Loading HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :stimulus, only: [:index]
+  resources :stimulus, only: [:index] do
+    collection do
+      get :clock
+    end
+  end
 
   root 'stimulus#index'
 end
